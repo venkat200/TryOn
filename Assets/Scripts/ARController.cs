52016e36cc683fc4dc97a32e4190d6fb7961394f
VENKAT <34069910+venkat200@users.noreply.github.com>
2020-12-06 18:17:18 +0530

    updates

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ARController : MonoBehaviour
 {
     public GameObject Object;
 
+    public GameObject LoadingPage_Panel;
+    public GameObject HomePage_Panel;
+
+    public GameObject Transition;
+
+
     private PlacementController placementController;
     // Start is called before the first frame update
     void Start()
     {
+        Screen.orientation = ScreenOrientation.Portrait;
+
         placementController = FindObjectOfType<PlacementController>();
+
+        /*
+        LoadingPage_Panel.SetActive(true);
+        HomePage_Panel.SetActive(false);
+
+        StartCoroutine(StartLoading());
+        */
     }
 
     // Update is called once per frame
@@ -22,4 +38,21 @@ public class ARController : MonoBehaviour
         }
     }
 
+    public IEnumerator StartLoading()
+    {
+        LoadingPage_Panel.SetActive(true);
+        yield return new WaitForSeconds(5);
+        LoadingPage_Panel.SetActive(false);
+
+        HomePage_Panel.SetActive(true);
+        Transition.SetActive(true);
+        Transition.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(1, 0, t));
+            Transition.GetComponent<Image>().color = newColor;
+            yield return null;
+        }
+        Transition.SetActive(false);
+    }
 }
