f59d12e1fb202c0d251861f62828d68b67a97866
VENKAT <34069910+venkat200@users.noreply.github.com>
2020-12-06 06:28:36 +0530

    update

---
+++ Assets/ScrollContent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollContent : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Screen.width > Screen.height)
+        {
+            if(Mathf.Abs(this.GetComponent<RectTransform>().offsetMax.y - this.GetComponent<RectTransform>().offsetMin.y - 723.0f) > 5)
+            {
+                this.GetComponent<RectTransform>().offsetMin = new Vector2(this.GetComponent<RectTransform>().offsetMin.x, -723.0f);
+                this.GetComponent<RectTransform>().offsetMax = new Vector2(this.GetComponent<RectTransform>().offsetMax.x, 0.0f);
+            }
+        }
+        else
+        {
+            if(Mathf.Abs(this.GetComponent<RectTransform>().offsetMax.y - this.GetComponent<RectTransform>().offsetMin.y - 1136.0f) > 5)
+            {
+                this.GetComponent<RectTransform>().offsetMin = new Vector2(this.GetComponent<RectTransform>().offsetMin.x, -1136.0f);
+                this.GetComponent<RectTransform>().offsetMax = new Vector2(this.GetComponent<RectTransform>().offsetMax.x, 0.0f);
+            }
+        }
+    }
+}
